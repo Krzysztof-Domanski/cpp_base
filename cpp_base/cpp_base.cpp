2e97a9ac8a911d6d49e24c165706263051613d15
Krzysztof Domanski <68679456+Krzysztof-Domanski@users.noreply.github.com>
2020-07-23 08:49:14 +0100

    Added a base C++ project using CMake

---
+++ cpp_base/cpp_base.cpp
@@ -0,0 +1,12 @@
+﻿// cpp_base.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_base.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ cpp_base/cpp_base.h
@@ -0,0 +1,8 @@
+﻿// cpp_base.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
